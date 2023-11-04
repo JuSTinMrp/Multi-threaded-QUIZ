e9935d73e1cf40553de293a745c05e16ab46f2b5
JuSTinMrp <r4mp3nt3st3r619@gmail.com>
2023-11-04 18:51:39 +0530

    fixing_fuking_errs

--- client.py
+++ client.py
@@ -3,29 +3,30 @@
 import socket
 
 SERVER_HOST = '127.0.0.1'
-SERVER_PORT = 7979
+SERVER_PORT = 1111
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((SERVER_HOST, SERVER_PORT))
 
-
 while True:
-    
-    name = client_socket.recv(1024).decode()
-    
-    
-    question = client_socket.recv(1024).decode()
-    if not question:
-        break
-    print(f'Question: {question}', end=' ')
-    
-    
-    for _ in range(4):
-        option = client_socket.recv(1024).decode()
-        print(option, end=' ')
+    name_prompt = client_socket.recv(1024).decode()
+    print(name_prompt, end=' ')
+
+    name = input()
+    client_socket.send(name.encode())    
+        
+    # question = client_socket.recv(1024).decode()
+    # if not question:
+    #     break
+    # print(f'Question: {question}', end=' ')
     
     
-    answer = input("\nAnswer: ")
-    client_socket.send(answer.encode())
+    # for _ in range(4):
+    #     option = client_socket.recv(1024).decode()
+    #     print(option, end=' ')
+    
+    
+    # answer = input("\nAnswer: ")
+    # client_socket.send(answer.encode())
 
 client_socket.close()
--- server.py
+++ server.py
@@ -5,16 +5,51 @@ import socket
 import threading
 import os
 
+
+def handle_client(client_socket):
+    
+    # Username and file creation
+    user_question="Enter your Name: "
+    client_socket.send(user_question.encode())
+    username = client_socket.recv(1024).decode()
+    
+    print(f'{username} connected')
+    # print (clients)
+
+    # user_file = f'{username}.txt'
+    # # ans_file = f'{username}_ans.txt'
+    
+    
+    # for question_data in data:
+        
+    #     question_with_options = f"{question_data['question']} [{', '.join(question_data['options'])}]"
+    #     client_socket.send(question_with_options.encode())
+
+        
+    #     answer = client_socket.recv(1024).decode()
+    #     print(f'{username} answered: {answer}')
+
+        
+    #     with open(user_file, 'a') as file:
+    #         file.write(f'Question: {question_data["question"]}\nAnswer: {answer}\nCorrect Answer: {question_data["correct_answer"]}\n\n')
+
+    #     # with open(ans_file, 'a') as file:
+    #     #     file.write(f'{answer}\n')
+
+    client_socket.close()
+    client_threads.remove(threading.current_thread())
+    
+    
 # IP and port number
 SERVER_HOST = '127.0.0.1'
-SERVER_PORT = 7979
+SERVER_PORT = 1111
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((SERVER_HOST, SERVER_PORT))
 server_socket.listen(5)
 
 
-clients = []
+client_threads = []
 
 # QUESTIONS
 data = [
@@ -31,50 +66,25 @@ data = [
     
 ]
 
-def handle_client(client_socket):
-    
-    # Username and file creation
-    client_socket.send("Enter your Name: ".encode())
-    username = client_socket.recv(1024).decode()
-    
-    print(f'{username} connected')
-    print (clients)
-
-    user_file = f'{username}.txt'
-    # ans_file = f'{username}_ans.txt'
-    
-    
-    for question_data in data:
-        
-        question_with_options = f"{question_data['question']} [{', '.join(question_data['options'])}]"
-        client_socket.send(question_with_options.encode())
-
-        
-        answer = client_socket.recv(1024).decode()
-        print(f'{username} answered: {answer}')
-
-        
-        with open(user_file, 'a') as file:
-            file.write(f'Question: {question_data["question"]}\nAnswer: {answer}\nCorrect Answer: {question_data["correct_answer"]}\n\n')
-
-        # with open(ans_file, 'a') as file:
-        #     file.write(f'{answer}\n')
-
-
-    client_socket.close()
-    clients.remove(client_socket)
-    del clients[username]
-    file.close()
-
 while True:
-    client, addr = server_socket.accept()
-    print(f'Accepted connection from {addr[0]}:{addr[1]}')
-    clients.append(client)
-    # clients['client'].append(client)
+    # print("Waiting for a client to connect...")
+    client_socket, client_address = server_socket.accept()
+    print(f"Accepted connection from {client_address}")
+    
+    # Create a new thread to handle the client
+    client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+    client_threads.append(client_thread)
+    client_thread.start()
 
-    # To join next client
-    client_handler = threading.Thread(target=handle_client(client))
-    client_handler.start()
+
+
+    # clients.remove(client_socket)
+    # del clients[username]
+    # file.close()
+    
+
+
+
 
 
 
