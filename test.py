362accdec35c335ab1204d84cef6c87711c5fac8
JuSTinMrp <r4mp3nt3st3r619@gmail.com>
2023-11-04 18:23:24 +0530

    modifying

--- test.py
+++ test.py
@@ -1,42 +1,72 @@
 import socket
+import threading
 
-SERVER_HOST = '0.0.0.0'  # Listen on all available network interfaces
-SERVER_PORT = 12345
+# Define the server address and port
+SERVER_ADDRESS = ('0.0.0.0', 12345)
 
-questions = [
+# Create a socket for the server
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(SERVER_ADDRESS)
+server_socket.listen(5)
+
+# List to store client threads and their scores
+client_threads = []
+client_scores = {}
+
+def handle_client(client_socket):
+    while True:
+        try:
+            data = client_socket.recv(1024).decode('utf-8')
+            if not data:
+                break
+            if data == 'REQUEST_QUESTION':
+                send_question(client_socket)
+            else:
+                handle_answer(client_socket, data)
+        except Exception as e:
+            print(f"Error handling client: {e}")
+            break
+
+    client_socket.close()
+
+def send_question(client_socket):
+    if len(quiz_questions) > 0:
+        question = quiz_questions.pop(0)  # Get the next question
+        client_socket.send(f"QUESTION:{question['question']}".encode('utf-8'))
+        for option in question['options']:
+            client_socket.send(option.encode('utf-8'))
+        client_socket.send(''.encode('utf-8'))  # Signal the end of options
+    else:
+        client_socket.send('QUIZ_OVER'.encode('utf-8'))
+
+def handle_answer(client_socket, answer):
+    # Check if the answer is correct
+    question = quiz_questions[0]
+    if answer == question['correct_answer']:
+        client_scores[client_socket] = client_scores.get(client_socket, 0) + 1
+
+# Question database (replace with your actual data)
+quiz_questions = [
     {
         'question': 'What is the capital of France?',
-        'options': ['a) London', 'b) Berlin', 'c) Paris', 'd) Madrid'],
-        'correct_answer': 'c'
+        'options': ['A) Paris', 'B) London', 'C) Berlin', 'D) Madrid'],
+        'correct_answer': 'A',
     },
     {
         'question': 'Which planet is known as the Red Planet?',
-        'options': ['a) Venus', 'b) Mars', 'c) Jupiter', 'd) Saturn'],
-        'correct_answer': 'b'
+        'options': ['A) Mars', 'B) Venus', 'C) Earth', 'D) Jupiter'],
+        'correct_answer': 'A',
     },
     # Add more questions here
 ]
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((SERVER_HOST, SERVER_PORT))
-server_socket.listen(5)
-
-print(f"Server is listening on {SERVER_HOST}:{SERVER_PORT}")
-
+# Start the server
 while True:
+    print("Waiting for a client to connect...")
     client_socket, client_address = server_socket.accept()
-    print(f"Accepted connection from {client_address[0]}:{client_address[1]}")
+    print(f"Accepted connection from {client_address}")
     
-    for question_data in questions:
-        # Send the question to the client
-        client_socket.send(question_data['question'].encode())
-        # Send the answer options to the client
-        for option in question_data['options']:
-            client_socket.send(option.encode())
-        
-        # Receive the client's answer
-        answer = client_socket.recv(1024).decode()
-        print(f"Client answered: {answer}")
-        # Check the answer and send feedback (you can add your logic here)
-
-    client_socket.close()
+    # Create a new thread to handle the client
+    client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+    client_threads.append(client_thread)
+    client_thread.start()
--- test1.py
+++ test1.py
@@ -1,28 +1,38 @@
 import socket
 
-SERVER_HOST = '0.0.0.0'  # Replace with the server's IP or hostname
-SERVER_PORT = 12345
+# Define the server address and port
+SERVER_ADDRESS = ('127.0.0.1', 12345)
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((SERVER_HOST, SERVER_PORT))
+def main():
+    # Create a socket for the client
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(SERVER_ADDRESS)
 
-while True:
-    question = client_socket.recv(1024).decode()
-    if not question:
-        break
-    print(f"Quiz Question: {question}")
-    
-    options = []
-    for _ in range(4):
-        option = client_socket.recv(1024).decode()
-        options.append(option)
-    
-    print("\n".join(options))
-    
-    answer = input("Your answer (e.g., 'a', 'b', 'c', 'd', or 'exit' to finish the quiz): ")
-    client_socket.send(answer.encode())
-    
-    if answer.lower() == 'exit':
-        break
+    while True:
+        try:
+            data = client_socket.recv(1024).decode('utf-8')
+            if not data:
+                break
+            if data.startswith('QUESTION:'):
+                print(data)  # Display the question
+                options = []
+                while True:
+                    option = client_socket.recv(1024).decode('utf-8')
+                    if not option:
+                        break
+                    options.append(option)
+                for option in options:
+                    print(option)
+                answer = input("Your answer: ").strip().upper()
+                client_socket.send(answer.encode('utf-8'))
+            elif data == 'QUIZ_OVER':
+                print("The quiz is over.")
+                break
+        except Exception as e:
+            print(f"Error in the quiz: {e}")
+            break
 
-client_socket.close()
+    client_socket.close()
+
+if __name__ == "__main__":
+    main()
